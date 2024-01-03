4bb81da37e4c556cdc5ab45e2b3e57dbc1a37fad
Ayoub EL Bouasri <ayoub.elbouuasri@gmail.com>
2024-01-03 10:32:06 +0300

    css & js changes

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const signUp = document.querySelector(".sign-up-card");
   const successSubscribe = document.querySelector(".success-subscribe-card");
   const dismissButton = document.querySelector(".dismiss-button");
+  const emailSubmitted = document.querySelector(".email-submitted");
 
   subscribeButton.addEventListener("click", () => {
     testEmail();
@@ -22,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
       emailInput.style.border = "1px solid #fa5a62";
       emailInput.style.color = "#fa5a62";
     } else {
+      emailSubmitted.innerText = email;
       signUp.style.display = "none";
       successSubscribe.style.display = "block";
     }
